2f60089cd770110d5dab40173de01d8d1469ecb6
Andrew <andrewvo1024@gmail.com>
2024-07-11 14:15:05 -0500

    small changes to event model

--- sip_and_play/event/models.py
+++ sip_and_play/event/models.py
@@ -7,7 +7,7 @@ class Event(models.Model):
     start_time = models.DateTimeField(blank=True, null=True)
     end_time = models.DateTimeField(blank=True, null=True)
     location = models.CharField(max_length=200, blank=True, null=True)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    # creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
