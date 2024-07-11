2e3c094e3aa39f6057b23f5b16b838523f80c013
Andrew <andrewvo1024@gmail.com>
2024-07-11 18:12:46 -0500

    fixed app.js

--- sip-n-play/src/App.js
+++ sip-n-play/src/App.js
@@ -1,28 +1,16 @@
 // src/App.js
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Home_Page from './components/home'
-import About_Page from './components/about';
-import Nav_Bar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
+import Home_Page from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Welcome to the testing site </h1>
-      </header>
-      <Nav_Bar />
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home_Page} />
-            <Route exact path="/about" component={About_Page} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home_Page />} />
+    </Routes>
+  );
+}
 
-    </div>
-  );          
-};
+
 
 export default App;
